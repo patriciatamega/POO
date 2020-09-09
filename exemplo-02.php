8475644207cfba6ca9112e62851522b2eb05a8eb
patriciatamega <pat.tamega@gmail.com>
2020-09-09 16:38:50 -0300

    Aula 43 - Atualizações

--- exemplo-02.php
+++ exemplo-02.php
@@ -2,8 +2,69 @@
 
 Class Carro{
 
-    
+    private $modelo;
+    private $motor;
+    private $ano;
+
+    public function getModelo() {//Pega Valor
+
+        return $this->modelo;
+
+    }
+
+    public function setModelo($modelo) {//Muda Valor
+
+        $this->modelo = $modelo;
+
+    }
+
+    public function getMotor():float {
+
+        return $this->motor;
+
+    }
+
+    public function setMotor($motor) {
+
+        $this->motor = $motor;
+
+    }
+
+    public function getAno():int {
+
+        return $this->ano;
+
+    }
+
+    public function setAno($ano) {
+
+        $this->ano = $ano;
+
+    }
+
+    public function exibir() {
+
+        return array(
+
+            "modelo"=>$this->getModelo(),
+            "motor"=>$this->getMotor(),
+            "ano"=>$this->getAno(),
+        );
+
+
+    }
 
 }
+
+$gol = new Carro();
+$gol->setModelo("Gol GT");
+$gol->setMotor("1.6");
+$gol->setAno("2021");
+
+print_r($gol->exibir());
+
+echo "<br>";
+
+var_dump($gol->exibir());
 
 ?>
